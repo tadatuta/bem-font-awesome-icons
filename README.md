61e0f7961fae7cd3d1288200ef67c409d1b40997
Vladimir Grinenko <i@tadatuta.com>
2017-03-10 01:38:06 +0300

    initial

---
+++ README.md
@@ -0,0 +1,74 @@
+# bem-font-awesome-icons
+
+[Font Awesome](http://fontawesome.io) icons extracted as SVG in BEM notation.
+
+## Content
+
+The library provides two modifiers for each icon:
+* `bg` to use SVG as background image
+* `glyph` to use inline SVG via templates (BEMHTML or BH)
+
+```
+icon/
+    _bg/
+        icon_bg_500px.css
+        icon_bg_500px.svg
+        icon_bg_address-book-o.css
+        icon_bg_address-book-o.svg
+        # and so on
+    _glyph/
+        icon_glyph_500px.bemhtml.js
+        icon_glyph_500px.bh.js
+        icon_glyph_address-book-o.bemhtml.js
+        icon_glyph_address-book-o.bh.js
+        # and so on
+```
+
+## Installation
+
+1. Add the library to project dependencies:
+    ```
+    npm i bem-font-awesome-icons --save
+    ```
+2. Add it as [redefinition level](https://en.bem.info/methodology/key-concepts/#redefinition-level)
+
+3. If you don't use [bem-components](https://en.bem.info/platform/libs/bem-components/) with `icon` block please add `icon.css` with something like this:
+
+```css
+.icon {
+    display: inline-block;
+
+    text-align: center;
+
+    background: 50% no-repeat;
+}
+
+/* Hack for correct baseline positioning */
+.icon:empty:after {
+    visibility: hidden;
+
+    content: '\00A0';
+}
+
+.icon > img,
+.icon > svg {
+    margin: -5.15em 0 -5em; /* 0.15 — magic number, empirically found */
+
+    vertical-align: middle;
+}
+```
+
+You're done :)
+
+## Usage
+
+### BEMJSON
+```js
+{ block: 'icon', mods: { bg: '500px' } }
+{ block: 'icon', mods: { glyph: '500px' } }
+```
+
+### HTML
+```html
+<div class="icon icon_bg_500px"></div>
+```
---
+++ generate.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const fontBlast = require('font-blast');
+const mv = require('mv');
+
+const BLOCK_NAME = 'icon';
+const MOD_SEPARATOR = '_';
+const MOD_NAME = 'glyph';
+const BG_MOD_NAME = 'bg';
+
+const blockWithModName = BLOCK_NAME + MOD_SEPARATOR + BG_MOD_NAME;
+
+const faSourceFolder = path.join('node_modules', 'font-awesome');
+
+fs.existsSync(BLOCK_NAME) || fs.mkdirSync(BLOCK_NAME);
+if (!fs.existsSync(path.join(BLOCK_NAME, MOD_SEPARATOR + MOD_NAME))) {
+    fs.mkdirSync(path.join(BLOCK_NAME, MOD_SEPARATOR + MOD_NAME));
+}
+if (!fs.existsSync(path.join(BLOCK_NAME, MOD_SEPARATOR + BG_MOD_NAME))) {
+    fs.mkdirSync(path.join(BLOCK_NAME, MOD_SEPARATOR + BG_MOD_NAME));
+}
+
+const selectorRegexp = /\.fa\-(.+)\:before/; // .fa-clock-o:before
+
+const css = fs.readFileSync(path.join(faSourceFolder, 'css', 'font-awesome.css'), 'utf8');
+
+const glyphMap = {};
+const fa = postcss.plugin('fa', function(options = {}) {
+    return function(css) {
+        css.walkRules(function(rule) {
+            const parsedSelector = selectorRegexp.exec(rule.selector);
+            if (!parsedSelector) return;
+
+            const modVal = parsedSelector[1];
+
+            const styleArr = [
+                '.' + blockWithModName + MOD_SEPARATOR + modVal + ' {'
+            ];
+
+            rule.walkDecls(function(decl, i) {
+                glyphMap[decl.value.slice(2, decl.value.length - 1)] = blockWithModName + MOD_SEPARATOR + modVal;
+                styleArr.push(decl.raws.before + '  background-image: url(' + blockWithModName + MOD_SEPARATOR + modVal + '.svg);');
+            });
+
+            styleArr.push('\n}');
+
+            fs.writeFileSync(path.join(BLOCK_NAME, MOD_SEPARATOR + BG_MOD_NAME, blockWithModName + MOD_SEPARATOR + modVal + '.css'), styleArr.join(''));
+        });
+    }
+});
+
+function getBhTmpl(modVal, svg) {
+    return `module.exports = function(bh) {
+    bh.match('icon_${MOD_NAME}_${modVal}', function(ctx) {
+        ctx.content('${svg}');
+    });
+};\n`
+}
+
+postcss([fa]).process(css).then(result => {
+    fontBlast(path.join(faSourceFolder, 'fonts', 'fontawesome-webfont.svg'), 'tmp', { filenames: glyphMap });
+
+    Promise.all(Object.keys(glyphMap).map(key => {
+        const filename = glyphMap[key] + '.svg';
+        const modVal = glyphMap[key].split(MOD_SEPARATOR).pop();
+        const svgContent = fs.readFileSync(path.join('tmp', 'svg', filename), 'utf8');
+
+        fs.writeFileSync(path.join(BLOCK_NAME, MOD_SEPARATOR + MOD_NAME, BLOCK_NAME + MOD_SEPARATOR + MOD_NAME + MOD_SEPARATOR + modVal + '.bemhtml.js'),
+            `block('icon').mod('${MOD_NAME}', '${modVal}').content()('${svgContent}');\n`);
+
+        fs.writeFileSync(path.join(BLOCK_NAME, MOD_SEPARATOR + MOD_NAME, BLOCK_NAME + MOD_SEPARATOR + MOD_NAME + MOD_SEPARATOR + modVal + '.bh.js'),
+            getBhTmpl(modVal, svgContent));
+
+        return mv(path.join('tmp', 'svg', filename), path.join(__dirname, BLOCK_NAME, MOD_SEPARATOR + BG_MOD_NAME, filename), err => {
+            if (err && err.code !== 'ENOENT') console.error(err);
+        });
+    })).then(() => console.log('Done'));
+});
